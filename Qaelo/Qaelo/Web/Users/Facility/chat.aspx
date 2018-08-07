78a4a23cdb2e7deae554f617f7ab942c6044e54e
tlekwadi <tlekwadi@retrorabbit.co.za>
2018-08-07 08:03:41 +0200

    Chat functionality for facility profile

---
+++ Qaelo/Qaelo/Web/Users/Facility/chat.aspx
@@ -0,0 +1,112 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="chat.aspx.cs" Inherits="Qaelo.Web.Users.Facility.chat" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Chat</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" type="text/css" rel="stylesheet" />
+    <%-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet"/>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        --%>
+
+      <link href="../../../Content/CustomCss/chat.css" rel="stylesheet" />   
+      <script type="application/x-javascript">
+        addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+               function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!-- //for-mobile-apps -->
+    <link href="../../../Template/css/bootstrap.css" rel="stylesheet" />
+    <link href="../../../Template/css/style.css" rel="stylesheet" />
+     <script src="../../../Scripts/jquery.min.js"></script>
+
+    <style>
+        .center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+    margin-top:150px;
+}
+    </style>
+</head>
+<body style="background:#efefe9;">
+    <form id="form1" runat="server">
+        <asp:ScriptManager ID="ScriptManager1" runat="server">
+        </asp:ScriptManager>
+
+            <!-- banner -->
+    <div class="accommodation">
+            <nav class="navbar navbar-default">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <div class="logo pull-right">
+                        <h3 style="margin-top:10px;"; ><a class="navbar-brand" href="../../../">Qaelo</a></h3>
+                    </div>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+                    <nav class="link-effect-2" id="link-effect-2">            
+                        <ul class="nav navbar-nav navbar-right">
+                        <li ><a href="Home.aspx">Clients</a></li>
+                             <li ><a href="list-facility.aspx">List a facility</a></li>
+                             <li ><a href="discount.aspx">Create discount</a></li>
+                             <li ><a href="manage-facilities.aspx">Manage Listings</a></li>
+                             <li ><a href="edit-profile.aspx">Edit Profile</a></li>
+                             <li ><a href="chat.aspx">Inbox</a></li>
+                        <li><a href="../../Account/tempLogin.aspx?logout=true">Logout</a></li>    			
+                    </ul>
+                    </nav>
+                </div>
+                <!-- /.navbar-collapse -->
+            </nav>
+    </div>
+    <!-- //banner -->
+
+    
+    <div class="container">
+<div class="messaging" style="margin-top:30px;background-color:whitesmoke">
+      <div class="inbox_msg">
+        <div class="inbox_people">
+          <div class="headind_srch">
+            <div class="recent_heading">
+              <h4>Chats</h4>
+            </div>
+          </div>
+          <div class="inbox_chat">
+              <asp:Label ID="lblChatList" runat="server" Text="No messages yet."></asp:Label>
+          </div>
+        </div>
+        <div class="mesgs">
+         <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                <fieldset>
+                   <div class="msg_history">
+                     <asp:Label ID="lblConversation" runat="server" Text=""></asp:Label>
+                   </div>
+
+
+          <div class="type_msg">
+            <div class="input_msg_write">
+                <asp:TextBox ID="txtMessage" runat="server" class="write_msg form-control" placeholder="Type a message" OnTextChanged="txtMessage_TextChanged" autocomplete="off"></asp:TextBox>
+                <asp:LinkButton ID="btnSendMessage" type="button" class="msg_send_btn" runat="server" Text="Button" OnClick="btnSendMessage_Click" ><i class="fa fa-paper-plane-o " aria-hidden="true"></i></asp:LinkButton>
+            </div>
+          </div>
+                           
+                </fieldset>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+        </div>
+      </div>
+      
+    </div></div>
+    </form>
+</body>
+</html>
---
+++ Qaelo/Qaelo/Web/Users/Facility/chat.aspx.cs
@@ -0,0 +1,209 @@
+﻿using Qaelo.Data;
+using Qaelo.Data.Accounts;
+using Qaelo.Data.ShopData;
+using Qaelo.Models.Inbox;
+using Qaelo.Models.ShopOwnerModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Qaelo.Web.Users.Facility
+{
+    public partial class chat : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if(Session["SHOPOWNER"] != null)
+                {
+                    //Get User loggedin ID
+                    string loggedInUserId = ((ShopOwner)Session["SHOPOWNER"]).Id + "F";
+
+                    //Load all messages 
+                    List<Message> messages = new MessageConnection().getAllMessages(loggedInUserId);
+
+                    //Group chats into conversations 
+                    List<Message> chats = new List<Message>();
+                    messages.Reverse();
+
+
+                    foreach (var item in messages)
+                    {
+                        if (!chats.Any(d => d.SenderID.Equals(item.SenderID)))
+                        {
+                            //Add the Last message 
+                            chats.Add(messages.Where(m => m.SenderID == item.SenderID).First());
+                        }
+                    }
+
+                    string chatList = "";
+                    string activeChat = "";
+                    string conversation = "";
+
+
+                    //Chat list on the left 
+                    foreach (var chat in chats){
+                        //Get all messages between the two people
+
+                        if (Request.QueryString["id"] != null)
+                        {
+                            if(Request.QueryString["id"].ToString().Equals(chat.SenderID))
+                                activeChat = "active_chat";
+                        }
+
+                        chatList += string.Format(@"<div class='chat_list {4}'><a href='chat.aspx?id={3}'>
+                          <div class='chat_people'>
+                            <div class='chat_img'> <img src='../../../Images/Profile/defaultProfilePic.jpg'/> </div>
+                            <div class='chat_ib'>
+                              <h5>{0}<span class='chat_date'>{1}</span></h5>
+                              <p>{2}</p>
+                            </div>
+                          </div></a>
+                        </div>", chat.NameFrom, chat.Date.ToShortDateString(),chat.Content, chat.SenderID, activeChat);
+
+                        activeChat = "";
+                    }
+
+                    lblChatList.Text = chatList;
+
+
+                    if(Request.QueryString["id"] != null)
+                    {
+                        //load all chats of this client 
+                        List<Message> myConversation = new MessageConnection().getConversation(Request.QueryString["id"].ToString(), loggedInUserId);
+
+                        foreach(var item in myConversation)
+                        {
+                            if (item.SenderID.Equals(loggedInUserId))
+                            {
+                                //sent 
+                                conversation += string.Format(@"
+                                <div class='outgoing_msg'>
+                                  <div class='sent_msg'>
+                                    <p> {0}</p>
+                                    <span class='time_date'> {1}    |    {2}</span> </div>
+                                </div>", item.Content,item.Date.ToShortTimeString(), item.Date.ToShortDateString());
+                            }
+                            else
+                            {
+                                //Received
+                                conversation += string.Format(@"<div class='incoming_msg'>
+                                  <div class='incoming_msg_img'> <img src='../../../Images/Profile/defaultProfilePic.jpg'/> </div>
+                                  <div class='received_msg'>
+                                    <div class='received_withd_msg'>
+                                      <p>{0}</p>
+                                      <span class='time_date'> {1}    |    {2}</span></div>
+                                  </div>
+                                </div>", item.Content, item.Date.ToShortTimeString(), item.Date.ToShortDateString());
+                            }
+                        }
+
+                        //Chat conversations
+                        lblConversation.Text = conversation;
+                    }
+                    else
+                    {
+                        //Chat conversations
+                        lblConversation.Text = "<img class='center' style='width:50%;height:50%' src='../../../Images/Inbox/defualt%20image.png'/>";
+                    }
+                }
+                else
+                {
+                    //Chat conversations
+                    Response.Redirect("~/Web/Account/tempLogin.aspx?page=Users/Facility/chat.aspx");
+                }
+            }
+        }
+
+        protected void btnSendMessage_Click(object sender, EventArgs e)
+        {
+            if(txtMessage.Text.Count() <= 0)
+            {
+                return;
+            }
+            string toId = "";
+            string text = txtMessage.Text;
+            ShopOwner facility = ((ShopOwner)Session["SHOPOWNER"]);
+            string loggedInUserId = facility.Id + "F";
+
+            //Load all messages
+            List<Message> messages = new MessageConnection().getAllMessages(loggedInUserId);
+
+
+            if (Request.QueryString["id"] != null)
+            {
+                toId = Request.QueryString["id"].ToString();
+                char letter = Request.QueryString["id"].ToString()[1];
+                string receiverName = "";
+
+                switch (letter)
+                {
+                    case 'F':
+                        string cf = Request.QueryString["id"][0].ToString();
+                        int fId = Convert.ToInt32(cf);
+                        receiverName = new ShopConnection().getShopOwner(fId).FullName;
+                        break;
+
+                    case 'C':
+                        string cc = Request.QueryString["id"][0].ToString();
+                        int cId = Convert.ToInt32(cc);
+                        receiverName = new AccountConnection().getStudent(cId).FirstName;
+                        break;
+                }
+
+                Message message = new Message(loggedInUserId, Request.QueryString["id"].ToString(), facility.FullName, receiverName, DateTime.Now, false, text);
+                new MessageConnection().sendMessage(message);
+            }
+            else
+            {
+                return;
+            }
+
+            txtMessage.Text = "";
+
+
+            //reload section 
+            string conversation = "";
+
+            List<Message> myConversation = new MessageConnection().getConversation(toId, loggedInUserId);
+
+            foreach (var item in myConversation)
+            {
+                if (item.SenderID.Equals(loggedInUserId))
+                {
+                    //sent 
+                    conversation += string.Format(@"
+                                <div class='outgoing_msg'>
+                                  <div class='sent_msg'>
+                                    <p> {0}</p>
+                                    <span class='time_date'> {1}    |    {2}</span> </div>
+                                </div>", item.Content, item.Date.ToShortTimeString(), item.Date.ToShortDateString());
+                }
+                else
+                {
+                    //Received
+                    conversation += string.Format(@"<div class='incoming_msg'>
+                                  <div class='incoming_msg_img'> <img src='../../../Images/Profile/defaultProfilePic.jpg'/> </div>
+                                  <div class='received_msg'>
+                                    <div class='received_withd_msg'>
+                                      <p>{0}</p>
+                                      <span class='time_date'> {1}    |    {2}</span></div>
+                                  </div>
+                                </div>", item.Content, item.Date.ToShortTimeString(), item.Date.ToShortDateString());
+                }
+            }
+
+            //Chat conversations
+            lblConversation.Text = conversation;
+        }
+
+        protected void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
